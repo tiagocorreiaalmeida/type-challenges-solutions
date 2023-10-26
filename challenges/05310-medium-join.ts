4943da1b42c73b647ccc9263a212bc81bc903a3e
tiagocorreiaalmeida <tiagoalmeida.correia@gmail.com>
2023-10-26 16:55:55 +0100

    feat(challenge-05310): add solution for challenge 05310-medium-join

---
+++ challenges/05310-medium-join.ts
@@ -0,0 +1,19 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  Expect<Equal<Join<["a", "p", "p", "l", "e"], "-">, "a-p-p-l-e">>,
+  Expect<Equal<Join<["Hello", "World"], " ">, "Hello World">>,
+  Expect<Equal<Join<["2", "2", "2"], 1>, "21212">>,
+  Expect<Equal<Join<["o"], "u">, "o">>
+];
+
+// ============= Your Code Here =============
+type Join<T extends string[], U extends string | number> = T extends [
+  infer Head extends string,
+  ...infer Tail extends string[]
+]
+  ? Tail extends []
+    ? Head
+    : `${Head}${U}${Join<Tail, U>}`
+  : "";
