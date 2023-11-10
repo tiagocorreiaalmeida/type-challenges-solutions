d8338f0f2b4a553c7b12dd44956249d0acd146fb
tiagocorreiaalmeida <tiagoalmeida.correia@gmail.com>
2023-11-10 14:10:45 +0000

    feat(challenge-00090): add solution for challenge 00090-hard-optional-keys

---
+++ challenges/00090-hard-optional-keys.ts
@@ -0,0 +1,22 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  Expect<Equal<OptionalKeys<{ a: number; b?: string }>, "b">>,
+  Expect<Equal<OptionalKeys<{ a: undefined; b?: undefined }>, "b">>,
+  Expect<
+    Equal<
+      OptionalKeys<{ a: undefined; b?: undefined; c?: string; d?: null }>,
+      "b" | "c" | "d"
+    >
+  >,
+  Expect<Equal<OptionalKeys<{}>, never>>
+];
+
+// ============= Your Code Here =============
+
+type GetOptional<T> = {
+  [key in keyof T as T[key] extends Required<T>[key] ? never : key]: T[key];
+};
+
+type OptionalKeys<T> = keyof GetOptional<T>;
