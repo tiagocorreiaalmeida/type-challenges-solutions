fcd5895625a2450fe95f8d81421db552955c683c
tiagocorreiaalmeida <tiagoalmeida.correia@gmail.com>
2023-11-14 15:30:52 +0000

    feat(challenge-04518): add solution for challenge 04518-medium-fill

---
+++ challenges/04518-medium-fill.ts
@@ -0,0 +1,36 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  Expect<Equal<Fill<[], 0>, []>>,
+  Expect<Equal<Fill<[], 0, 0, 3>, []>>,
+  Expect<Equal<Fill<[1, 2, 3], 0, 0, 0>, [1, 2, 3]>>,
+  Expect<Equal<Fill<[1, 2, 3], 0, 2, 2>, [1, 2, 3]>>,
+  Expect<Equal<Fill<[1, 2, 3], 0>, [0, 0, 0]>>,
+  Expect<Equal<Fill<[1, 2, 3], true>, [true, true, true]>>,
+  Expect<Equal<Fill<[1, 2, 3], true, 0, 1>, [true, 2, 3]>>,
+  Expect<Equal<Fill<[1, 2, 3], true, 1, 3>, [1, true, true]>>,
+  Expect<Equal<Fill<[1, 2, 3], true, 10, 0>, [1, 2, 3]>>,
+  Expect<Equal<Fill<[1, 2, 3], true, 10, 20>, [1, 2, 3]>>,
+  Expect<Equal<Fill<[1, 2, 3], true, 0, 10>, [true, true, true]>>
+];
+
+// ============= Your Code Here =============
+type Fill<
+  T extends unknown[],
+  N,
+  Start extends number = 0,
+  End extends number = T["length"],
+  Acc extends unknown[] = [],
+  IsFillRange = false
+> = Start extends End
+  ? T
+  : T extends [infer Head, ...infer Tail]
+  ? IsFillRange extends true
+    ? Acc["length"] extends End
+      ? [...Acc, Head, ...Tail]
+      : Fill<Tail, N, Start, End, [...Acc, N], true>
+    : Acc["length"] extends Start
+    ? Fill<Tail, N, Start, End, [...Acc, N], true>
+    : Fill<Tail, N, Start, End, [...Acc, Head], false>
+  : Acc;
